301a019dad55c2c0f1b864b58f2b537874761e1d
Keven Pereira <xrl8k9lx@gmail.com>
2025-08-15 13:29:40 -0300

    Update app.py

--- flask-crud-sqlite/app.py
+++ flask-crud-sqlite/app.py
@@ -37,7 +37,7 @@ def get_all_users():
     return usuarios
 
 # ======== Rotas ========
-# Página inicial: ver usuários e formulário de cadastro
+# Página inicial: formulário de cadastro
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
